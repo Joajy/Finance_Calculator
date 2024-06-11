7d4847a8f9d48a48552c69b8658df31e4fdcd1d2
Joajy <joajy98@gmail.com>
2024-06-12 00:38:46 +0900

    fix: loan type error corrected(yet epa)

--- src/main/java/com/finance/calculator/constant/LoanType.java
+++ src/main/java/com/finance/calculator/constant/LoanType.java
@@ -8,23 +8,28 @@ public class LoanType {
 //        if(s.equals("eia")) return Math.pow(1 + rate, year);
         if(s.equals("eia")) return eia(rate, year);
         //원금균등분할상환
-        else if(s.equals("epa")) return Math.pow(1 + rate, year);
+        else if(s.equals("epa")) return epa(rate, year);
         //만기일시상환(bullet)
-        return 1 + rate * year;
+        return bullet(rate, year);
     }
 
     private static double eia(double rate, int year) {
         double monthlyRate = rate / 12;
-        double month = year * 12;
+        int month = year * 12;
         double denominator = Math.pow((1 + monthlyRate), month);
         return monthlyRate * denominator / (denominator - 1);
     }
 
+    //수정 필요
     private static double epa(double rate, int year) {
         double monthlyRate = rate / 12;
-        double month = year * 12;
-        double denominator = Math.pow((1 + monthlyRate), month);
-        return monthlyRate * denominator / (denominator - 1);
+        int month = year * 12;
+        return (1 / month) + monthlyRate;
     }
 
+    private static double bullet(double rate, int year) {
+        double monthlyRate = rate / 12;
+        int month = year * 12;
+        return (1 / month) + monthlyRate;
+    }
 }
